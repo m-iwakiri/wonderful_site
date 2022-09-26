d786c6f826ba37e0d09fac86b6c0572f62e903bb
iwakiri <m.iwakiri@office-iwakiri.com>
2022-09-26 19:12:11 +0900

    20220926

--- js/index.js
+++ js/index.js
@@ -10,4 +10,14 @@ const sp_navi = document.getElementById("sp_navi");
 menu_open.addEventListener("click", () => {
   //   sp_navi.classList.add("open");
   sp_navi.classList.toggle("open");
+  menu_open.classList.toggle("open");
+});
+
+const move_top = document.getElementById("move_top");
+move_top.addEventListener("click", (e) => {
+  e.preventDefault();
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
 });
